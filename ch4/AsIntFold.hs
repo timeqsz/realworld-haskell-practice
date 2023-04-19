81817d72bf9531cfd65fe7caa8041410b766a2ae
timeqsz <freed7333@gmail.com>
2023-04-19 23:56:22 +0800

    add myconcat

--- ch4/AsIntFold.hs
+++ ch4/AsIntFold.hs
@@ -1,9 +1,14 @@
 import Data.Char
 import Data.List
 
+type ErrorMessage = String
+
 asIntFold :: String -> Int
 asIntFold [] = 0
 asIntFold l@(x:xs) = case x of
     '-' -> - (foldl' f 0 xs)
     _   -> foldl' f 0 l
   where f acc x = acc * 10 + digitToInt x
+
+myconcat :: [[a]] -> [a]
+myconcat a = foldr (++) [] a
